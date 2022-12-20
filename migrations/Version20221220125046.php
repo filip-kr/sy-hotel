dcaf75b4cbbd2e336666bea2b50371dc91ea54cd
Filip Krnjaković <f.krnja@gmail.com>
2022-12-20 13:56:33 +0100

    Minor template tweaks, migrations cleanup

---
+++ migrations/Version20221220125046.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221220125046 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE guest (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, dob DATETIME NOT NULL, country VARCHAR(255) NOT NULL, oib CHAR(11) DEFAULT NULL, passport_number VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_ACB79A35E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE overnight_stay (id INT AUTO_INCREMENT NOT NULL, room_id INT NOT NULL, total_price NUMERIC(5, 2) NOT NULL, is_active TINYINT(1) DEFAULT NULL, INDEX IDX_F78F23A54177093 (room_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, guest_id INT NOT NULL, overnight_stay_id INT DEFAULT NULL, sign_in_date DATETIME NOT NULL, sign_out_date DATETIME NOT NULL, INDEX IDX_42C849559A4AA658 (guest_id), UNIQUE INDEX UNIQ_42C8495537E9DF48 (overnight_stay_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE room (id INT AUTO_INCREMENT NOT NULL, number INT NOT NULL, number_of_beds INT NOT NULL, description VARCHAR(255) DEFAULT NULL, price INT NOT NULL, UNIQUE INDEX UNIQ_729F519B96901F54 (number), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE overnight_stay ADD CONSTRAINT FK_F78F23A54177093 FOREIGN KEY (room_id) REFERENCES room (id)');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C849559A4AA658 FOREIGN KEY (guest_id) REFERENCES guest (id)');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C8495537E9DF48 FOREIGN KEY (overnight_stay_id) REFERENCES overnight_stay (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE overnight_stay DROP FOREIGN KEY FK_F78F23A54177093');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C849559A4AA658');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C8495537E9DF48');
+        $this->addSql('DROP TABLE guest');
+        $this->addSql('DROP TABLE overnight_stay');
+        $this->addSql('DROP TABLE reservation');
+        $this->addSql('DROP TABLE room');
+        $this->addSql('DROP TABLE user');
+    }
+}
